325a0be9c7b1a3f15c3fdc7588d3ee990b2f6c98
baggakunal <baggakunal93@gmail.com>
2020-04-11 02:57:04 +0530

    docstrings and comments

--- src/words.py
+++ src/words.py
@@ -1,9 +1,26 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+    python3 words.py <URL>
+    Sample url: http://sixty-north.com/c/t.txt
+"""
+
 import sys
 from urllib.request import urlopen
 
-# URL: 'http://sixty-north.com/c/t.txt'
 
 def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of string containing the words from the document.
+
+    Raises:
+        None
+    """
     story = urlopen(url);
     story_words = []
     for line in story:
@@ -21,6 +38,11 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """Print items in the same line.
+
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         if item == '\n':
             print(item, sep='', end='')
@@ -29,9 +51,14 @@ def print_items(items):
 
 
 def main(url):
+    """Print each word from a text document retrieved from the url.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1]) # The 0th arg is the module filename.
