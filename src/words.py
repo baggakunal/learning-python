43ea2aadc678baea565457a888549bda2d913b0c
baggakunal <baggakunal93@gmail.com>
2020-04-11 02:04:48 +0530

    understanding command line arguments

--- src/words.py
+++ src/words.py
@@ -1,17 +1,37 @@
+import sys
 from urllib.request import urlopen
 
+# URL: 'http://sixty-north.com/c/t.txt'
 
-def fetch_words():
-    story = urlopen('http://sixty-north.com/c/t.txt');
+def fetch_words(url):
+    story = urlopen(url);
     story_words = []
     for line in story:
         line_words = line.decode('utf8').split()
+        first = True
         for word in line_words:
-            story_words.append(word)
+            if first == True:
+                story_words.append(word.capitalize())
+                first = False
+            else:
+                story_words.append(word)
+        story_words.append('\n')
     story.close();
-    for word in story_words:
-        print(word)
+    return story_words
+
+
+def print_items(items):
+    for item in items:
+        if item == '\n':
+            print(item, sep='', end='')
+        else:
+            print(item, sep='', end=' ')
+
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
 
 
 if __name__ == '__main__':
-    fetch_words()
+    main(sys.argv[1])
