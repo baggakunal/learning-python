d41f36360c5492bdb416bed62d2cba74848d8810
baggakunal <baggakunal93@gmail.com>
2020-04-11 01:22:13 +0530

    understanding __name__

--- src/words.py
+++ src/words.py
@@ -1,16 +1,17 @@
 from urllib.request import urlopen
 
 
-story = urlopen('http://sixty-north.com/c/t.txt');
-story_words = []
-for line in story:
-    line_words = line.decode('utf8').split()
-    for word in line_words:
-        story_words.append(word)
-story.close();
-for word in story_words:
-    print(word)
+def fetch_words():
+    story = urlopen('http://sixty-north.com/c/t.txt');
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf8').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close();
+    for word in story_words:
+        print(word)
 
 
-def square(x):
-    return x * x
+if __name__ == '__main__':
+    fetch_words()
